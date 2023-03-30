55d897f7fc087656927934c0144c1c10ce1d54d0
Abdirashidnoor <abdirashid16112@gmail.com>
2023-03-30 07:35:30 +0300

    assingment done

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const chars = word.split("");
+
+  // Initialize two pointers,
+  let left = 0;
+  let right = chars.length - 1;
+
+  while (left < right) {
+    if (chars[left] !== chars[right]) {
+      return false;
+    } left++; right--;
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  1.Convert the input string to an array of characters
+ 2.Initialize two pointers, one at the start of the array and one at the end
+  3.While the pointers haven't crossed each other:
+      .If the characters at the pointers don't match, return false
+      .Move the pointers towards each other
+ 4.If we made it through the loop, the string is a palindrome, so return true
 */
 
 /*
